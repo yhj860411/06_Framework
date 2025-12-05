a6f930ad306da8cb438232bd71b31879023384c0
yhj860411 <yhj860411@gmail.com>
2025-12-05 17:07:33 +0900

    12/5실습

---
+++ boardProject/src/main/java/edu/kh/project/common/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package edu.kh.project.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*
+ * @Configuration
+ * - 설정용 클래스임을 명시
+ * + 객체로 생성해서 내부 코드를 서버 실행 시 모두 수행
+ * 
+ * 
+ */
+
+@Configuration
+public class SecurityConfig {
+	
+	// BCryptPasswordEncoder : 평문을 BCrypt 패턴을 이용하여 암호화
+	//							평문과 암호화된문자열을 비교해서
+	//							서로 맞는 값인지 판단해줌
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ boardProject/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -0,0 +1,17 @@
+package edu.kh.project.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/") // "/" 요청 매핑
+	public String mainPage() {
+		
+		// 접두사/접미사 제외
+		// classpath:/templates/
+		// .html
+		return "common/main"; // forward
+	}
+}
